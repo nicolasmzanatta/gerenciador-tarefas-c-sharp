3ce06176c597301a7108615c1e84bba6a00ec5c8
Nicolas Moreira Zanatta <nicolas.mzanatta93@gmail.com>
2021-06-01 16:13:26 -0300

    aula 2 projeto backend

--- GerenciadorDeTarefas/GerenciadorDeTarefas/Controllers/LoginController.cs
+++ GerenciadorDeTarefas/GerenciadorDeTarefas/Controllers/LoginController.cs
@@ -1,4 +1,7 @@
 ﻿using GerenciadorDeTarefas.Dtos;
+using GerenciadorDeTarefas.Models;
+using GerenciadorDeTarefas.Services;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
@@ -11,9 +14,14 @@ namespace GerenciadorDeTarefas.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class LoginController : ControllerBase
+    public class LoginController : BaseController
     {
         private readonly ILogger<LoginController> _logger; //atributos privados sempre com underscore frente
+
+        private readonly string loginTeste = "admin@admin.com";
+        private readonly string senhaTeste = "admin1234@";
+
+
 
         public LoginController(ILogger<LoginController> logger)
         {
@@ -21,11 +29,15 @@ namespace GerenciadorDeTarefas.Controllers
         }
 
         [HttpPost]
+        [AllowAnonymous]
         public IActionResult EfetuarLogin([FromBody] LoginRequisicaoDto requisicao)
         {
             try
             {
-                if(requisicao == null || requisicao.Login == null || requisicao.Senha == null)
+                if (requisicao == null
+                    || string.IsNullOrEmpty(requisicao.Login) || string.IsNullOrWhiteSpace(requisicao.Login)
+                    || string.IsNullOrEmpty(requisicao.Senha) || string.IsNullOrWhiteSpace(requisicao.Senha)
+                    || requisicao.Login != loginTeste || requisicao.Senha != senhaTeste)
                 {
                     return BadRequest(new ErroRespotaDto()
                     {
@@ -34,12 +46,28 @@ namespace GerenciadorDeTarefas.Controllers
                     });
                 }
 
-                return Ok("Usuário autenticado com sucesso");
+                var usuarioTeste = new Usuario()
+                {
+                    Id = 1,
+                    Nome = "Usuário de Teste",
+                    Email = loginTeste,
+                    Senha = senhaTeste
+                };
+
+                var token = TokenService.CriarToken(usuarioTeste);
+
+                return Ok(new LoginRespostaDto()
+                {
+                    Email = usuarioTeste.Email,
+                    Nome = usuarioTeste.Nome,
+                    Token = token
+                });
             }
-            catch(Exception excecao)
+            catch (Exception excecao)
             {
                 _logger.LogError($"Ocorreu erro ao efetuar login: {excecao.Message}", excecao);
-                return StatusCode(StatusCodes.Status500InternalServerError, new ErroRespotaDto() { 
+                return StatusCode(StatusCodes.Status500InternalServerError, new ErroRespotaDto()
+                {
                     Status = StatusCodes.Status500InternalServerError,
                     Erro = "Ocorreu erro ao efetuar login, tente novamente!"
                 });
